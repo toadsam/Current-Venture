b37aa6cb4e53b945fbc8e67edcced8a69226ced9
toadsam <75034052+toadsam@users.noreply.github.com>
2024-02-13 21:05:54 +0900

    [ADD] 플레이어 떨어지는 애니메이션 추가(조금 이상합니다....) (-재훈)

--- Assets/Scripts/Characters/Player/PlayerGroundedState.cs
+++ Assets/Scripts/Characters/Player/PlayerGroundedState.cs
@@ -29,6 +29,12 @@ public class PlayerGroundedState : PlayerBaseState
     public override void PhysicsUpdate()
     {
         base.PhysicsUpdate();
+        if (!stateMachine.Player.Controller.isGrounded
+        && stateMachine.Player.Controller.velocity.y < Physics.gravity.y * Time.fixedDeltaTime)
+        {
+            stateMachine.ChangeState(stateMachine.FallState);
+            return;
+        }
     }
     protected override void OnMovementCanceled(InputAction.CallbackContext context)
     {
